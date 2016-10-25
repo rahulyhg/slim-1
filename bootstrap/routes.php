33f3312ff49f2b990f8f381b55480068c6aa86bf
Ivan Dokov <ivangdokov@gmail.com>
2016-10-25 10:30:10 +0300

    Add names to routes

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -1,4 +1,4 @@
 <?php
 
-$app->get('/', '\App\Controllers\IndexController');
-$app->get('/test', '\App\Controllers\IndexController');
+$app->get('/', '\App\Controllers\IndexController')->setName('index');
+$app->get('/test', '\App\Controllers\IndexController')->setName('test');
