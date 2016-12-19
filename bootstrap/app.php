bf9b1a4859894b12d3735281fe4d7973125fbca1
Ivan Dokov <ivangdokov@gmail.com>
2016-12-19 14:56:45 +0200

    Removed default session_start() in app.php because slim/csrf 0.8.0 fixes the session issue and it can be started from SessionMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-/**
- * Waiting for PR to get accepted and this session_start will be removed
- * because the RKA\SessionMiddleware will register the session
- * https://github.com/slimphp/Slim-Csrf/pull/55
- */
-session_start();
-
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
