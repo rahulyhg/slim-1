f2996305eb49ba2385143d1fe04ab6def0bda1f5
Ivan Dokov <ivangdokov@gmail.com>
2017-01-11 10:42:32 +0200

    Removed lessql dependency as it may not be required by many projects

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -101,14 +101,6 @@ $container['oldInput'] = function ($container) {
     return new App\Middlewares\OldInput($container);
 };
 
-/** @var LessQL\Database */
-$container['db'] = function () {
-    $dns = sprintf("%s:dbname=%s;host=%s", env('DB_TYPE'), env('DB_NAME'), env('DB_HOST'));
-    $user = env('DB_USER');
-    $pass = env('DB_PASS');
-    return new LessQL\Database(new PDO($dns, $user, $pass));
-};
-
 /** @var RKA\Session */
 $container['session'] = function () {
     return new RKA\Session;
